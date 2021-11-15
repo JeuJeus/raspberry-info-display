987d5c1b6cfb82a6ffe3ad5a2651ac0f814e10ea
Julius Figge <julius@figge.eu>
2021-11-15 17:31:18 +0100

    introduce config

--- src/Clock.js
+++ src/Clock.js
@@ -1,11 +1,12 @@
 import {useEffect, useState} from "react";
 import style from './Clock.module.css';
+import {TIME_LOCALE} from "./config";
 
 const Clock = () => {
 
     const [time, setTime] = useState();
 
-    const buildDateString = date => date.toLocaleTimeString('de-DE', {hour12: false});
+    const buildDateString = date => date.toLocaleTimeString(TIME_LOCALE, {hour12: false});
 
     useEffect(() => {
         setInterval(() => {
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const TIME_LOCALE = 'de-DE';
+export const LOCATION_LATITUDE = '52.02';
+export const LOCATION_LONGITUDE = '8.05';
