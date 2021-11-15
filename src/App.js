8a5991edd3e7cf9f34ac64f805d6452d1e99f5d4
Julius Figge <julius@figge.eu>
2021-11-15 17:29:27 +0100

    introduce weather component body, refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import style from './App.module.css';
 import Clock from "./Clock";
+import Weather from "./Weather";
 
 function App() {
-  return (
-    <div className={style.app}>
-      <Clock/>
-    </div>
-  );
+    return (
+        <div className={style.app}>
+            <Clock/>
+            <Weather/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import style from './Weather.module.css'
+
+const Weather = () => {
+    return (
+     <div className={style.weather}>wetter</div>
+    );
+};
+
+export default Weather;
