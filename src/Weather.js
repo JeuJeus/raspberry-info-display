218941cb6c273ad9674231d69ec6caccd35809aa
Julius Figge <julius@figge.eu>
2021-11-16 08:39:03 +0100

    wip icons with font awesome

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,10 @@
 import style from './Weather.module.css'
 import {LOCATION_LATITUDE, LOCATION_LONGITUDE} from "./config";
 import {useEffect, useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTemperatureHigh, faTint, faWind, faSun, faMoon, faCloudSun, faCloudMoon, faCloud, faSmog, faCloudRain, faCloudSnow, faSnowflake, faCloudHail, faCloudLightning} from "@fortawesome/free-solid-svg-icons";
+import {weatherIcons} from "./weatherIconEnum";
+
 
 const Weather = () => {
 
@@ -31,11 +35,11 @@ const Weather = () => {
     return (
         <div className={style.weather}>
             {weather && <div className={style.weatherDetails}>
-                <div>Temperature: {weather.temperature} C°</div>
-                <div>Pressure: {weather.pressure_msl} hPa</div>
-                <div>Humidity: {weather.relative_humidity} %</div>
+                <div><FontAwesomeIcon icon={faTemperatureHigh}/> {weather.temperature} C°</div>
+                <div><FontAwesomeIcon icon={faWind}/> {weather.pressure_msl} hPa</div>
+                <div><FontAwesomeIcon icon={faTint}/> {weather.relative_humidity} %</div>
                 <div>Condition: {weather.condition}</div>
-                <div>Icon: {weather.icon}</div>
+                <div>Icon: <FontAwesomeIcon icon={weatherIcons[weather.icon]}/>{weatherIcons[weather.icon]}</div>
             </div>}
         </div>
     );
