9b496d0d43dccfc5b700072796ed55c83ff42129
Julius Figge <julius@figge.eu>
2021-11-17 08:36:41 +0100

    generify implementation of weather details in preperation of forecast feature

--- src/weather/CurrentWeather.js
+++ src/weather/CurrentWeather.js
@@ -1,11 +1,7 @@
 import style from './CurrentWeather.module.css'
 import {useEffect, useState} from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHandHoldingWater, faTemperatureHigh, faTint, faWind} from "@fortawesome/free-solid-svg-icons";
-import WeatherIcon from "./WeatherIcon";
 import {fetchCurrentWeather} from "./weatherDataFetcher";
-import {weatherIcons} from "./weatherIconEnum";
-import {between} from "../helper/helper";
+import {WeatherDataDisplay} from "./WeatherDataDisplay";
 
 
 const CurrentWeather = () => {
@@ -22,29 +18,10 @@ const CurrentWeather = () => {
         }, 1000 * 60 * 15);
     })
 
-    const isRaining = () => {
-        return weather.condition === weatherIcons.RAIN
-            || weather.condition === weatherIcons.SLEET
-            || weather.condition === weatherIcons.SNOW
-            || weather.condition === weatherIcons.HAIL
-            || weather.condition === weatherIcons.THUNDERSTORM;
-    };
-
     return (
         <div className={style.weather}>
-            {weather && <div className={style.weatherDetails}>
-                <div className={style.weatherData}>
-                    {/*generify this to be included into forecast*/}
-                    <div><FontAwesomeIcon icon={faTemperatureHigh}/> {weather.temperature} C°</div>
-                    <div><FontAwesomeIcon icon={faWind}/> {weather.pressure_msl} hPa</div>
-                    <div><FontAwesomeIcon icon={faTint}/> {weather.relative_humidity} %</div>
-                    {isRaining() && <div><FontAwesomeIcon icon={faHandHoldingWater}/> {weather.precipitation_60} %</div>}
-                    <div><FontAwesomeIcon icon={faTint} /> {weather.relative_humidity} %</div>
-                    {/*    TODO insert icon for wind direction + speed*/}
-                </div>
-                <div className={style.momentaryWeatherIcon}>
-                    <div><WeatherIcon icon={weather.icon}/></div>
-                </div>
+            {weather && <div>
+                <WeatherDataDisplay weather={weather}/>
             </div>}
         </div>
     );
---
+++ src/weather/WeatherDataDisplay.js
@@ -0,0 +1,40 @@
+import style from "./CurrentWeather.module.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faHandHoldingWater, faLocationArrow,
+    faTachometerAlt,
+    faTemperatureHigh,
+    faTint,
+    faWind
+} from "@fortawesome/free-solid-svg-icons";
+import WeatherIcon from "./WeatherIcon";
+import {weatherIcons} from "./weatherIconEnum";
+
+export const WeatherDataDisplay = (props) => {
+
+    const isRaining = () => {
+        return props.weather.condition === weatherIcons.RAIN
+            || props.weather.condition === weatherIcons.SLEET
+            || props.weather.condition === weatherIcons.SNOW
+            || props.weather.condition === weatherIcons.HAIL
+            || props.weather.condition === weatherIcons.THUNDERSTORM;
+    };
+
+    return (
+        <div className={style.weatherDetails}>
+            <div className={style.weatherData}>
+                <div><FontAwesomeIcon icon={faTemperatureHigh}/> {props.weather.temperature} C°</div>
+                <div><FontAwesomeIcon icon={faTachometerAlt}/> {props.weather.pressure_msl} hPa</div>
+                <div><FontAwesomeIcon icon={faTint}/> {props.weather.relative_humidity} %</div>
+                {isRaining() &&
+                <div><FontAwesomeIcon icon={faHandHoldingWater}/> {props.weather.precipitation_60} %</div>}
+                <div><FontAwesomeIcon icon={faWind}/> {props.weather.wind_speed_60} km/h <FontAwesomeIcon
+                    icon={faLocationArrow} style={{transform: `rotate(${props.weather.wind_direction_60}deg)`}}/></div>
+            </div>
+            <div className={style.momentaryWeatherIcon}>
+                <div><WeatherIcon icon={props.weather.icon}/></div>
+            </div>
+        </div>
+    );
+
+}
