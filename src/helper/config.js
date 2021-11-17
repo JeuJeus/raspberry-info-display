d9afe539a562de47616c3855c80ea2a6f1b2a2a7
Julius Figge <julius@figge.eu>
2021-11-17 08:39:18 +0100

    introduce environment variable mapping to config variables

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,3 +1,3 @@
-export const TIME_LOCALE = 'de-DE';
-export const LOCATION_LATITUDE = '52.0714';
-export const LOCATION_LONGITUDE = '8.4565';
+export const TIME_LOCALE = process.env.TIME_LOCALE || 'de-DE';
+export const LOCATION_LATITUDE = process.env.LOCATION_LATITUDE ||'52.0714';
+export const LOCATION_LONGITUDE = process.env.LOCATION_LONGITUDE || '8.4565';
