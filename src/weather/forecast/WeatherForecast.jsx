41342df809e3105599bcdb8ee4fea3ba4eff2a5e
Julius Figge <julius@figge.eu>
2021-11-18 21:43:01 +0100

    WIP - not working

--- src/weather/forecast/WeatherForecast.jsx
+++ src/weather/forecast/WeatherForecast.jsx
@@ -1,9 +1,20 @@
 import style from './WeatherForecast.module.css';
 import {useEffect, useState} from "react";
-import {fetchCurrentWeather, fetchWeatherForecast} from "../weatherDataFetcher";
-import {WeatherDataDisplay} from "../dataDisplay/WeatherDataDisplay";
+import {fetchWeatherForecast} from "../weatherDataFetcher";
 import {parseForecast} from "./forecastParser";
 
+
+const iterateTroughDays = () => {
+
+};
+
+const displayEachDay = (forecast) => {
+    return Object.entries(forecast).filter(X => X).map(([day, forecastData]) =>
+        <div>
+            <div className={style.day} key={day}>{day}</div>
+        </div>
+    )
+}
 
 export const WeatherForecast = () => {
 
@@ -13,16 +24,20 @@ export const WeatherForecast = () => {
 
     useEffect(() => {
         if (forecast === undefined) getForecast();
+        else console.log(forecast)
         setInterval(() => {
             getForecast();
         }, 1000 * 60 * 60);
+        setInterval(() => {
+            iterateTroughDays();
+        }, 1000 * 1 / 2);
     })
 
     return (
-        <div className={style.weatherForecast}>
-            {/*{forecast.map(day =>(*/}
-            {/*    <WeatherDataDisplay key={day.dt} weather={day}/>*/}
-            {/*))}*/}
+        <div>
+            {forecast && <div className={style.weatherForecast}>
+                {displayEachDay(forecast)}
+            </div>}
         </div>
     );
 }
