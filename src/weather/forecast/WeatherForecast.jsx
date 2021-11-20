2f7178c0c7d8b4c7d7a71aeb55b6b2dd6aa10fb0
Julius Figge <julius@figge.eu>
2021-11-20 15:00:22 +0100

    format date displayed

--- src/weather/forecast/WeatherForecast.jsx
+++ src/weather/forecast/WeatherForecast.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import {fetchWeatherForecast} from "../weatherDataFetcher";
 import {parseForecast} from "./forecastParser";
 import {WeatherDataDisplay} from "../dataDisplay/WeatherDataDisplay";
+import {TIME_LOCALE} from "../../helper/config";
 
 
 export const WeatherForecast = () => {
@@ -32,20 +33,22 @@ export const WeatherForecast = () => {
         saveCurrentTimeShown((currentTimeShown + 1) % HOURS_PER_DAY)
     };
 
-    const displayEachDay = (forecast) => {
-        return Object.entries(forecast).filter(X => X).map(([day, forecastData]) =>
-            <div>
-                <div className={style.day}
-                     key={`${day}timestamp`}>{day} {forecastData[currentTimeShown].timestamp}</div>
-                <WeatherDataDisplay key={`${day}data`} weather={forecastData[currentTimeShown]}/>
-            </div>
+    const displayEachDay = () => {
+        return Object.entries(forecast).filter(X => X).map(([day, forecastData]) => {
+            let dateAndTimeFormatted = new Date(forecastData[currentTimeShown].timestamp).toLocaleString(TIME_LOCALE);
+            return <div>
+                    <div className={style.day}
+                         key={`${day}timestamp`}>{dateAndTimeFormatted}</div>
+                    <WeatherDataDisplay key={`${day}data`} weather={forecastData[currentTimeShown]}/>
+                </div>;
+            }
         )
     }
 
     return (
         <div>
             {forecast && <div className={style.weatherForecast}>
-                {displayEachDay(forecast)}
+                {displayEachDay()}
             </div>}
         </div>
     );
