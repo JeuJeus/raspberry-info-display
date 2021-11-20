c9ccafb1a679a351525e74512809d8573891d281
Julius Figge <julius@figge.eu>
2021-11-20 14:56:43 +0100

    got iterating trough days working

--- src/weather/forecast/WeatherForecast.jsx
+++ src/weather/forecast/WeatherForecast.jsx
@@ -2,35 +2,44 @@ import style from './WeatherForecast.module.css';
 import {useEffect, useState} from "react";
 import {fetchWeatherForecast} from "../weatherDataFetcher";
 import {parseForecast} from "./forecastParser";
+import {WeatherDataDisplay} from "../dataDisplay/WeatherDataDisplay";
 
-
-const iterateTroughDays = (forecast) => {
-
-};
-
-const displayEachDay = (forecast) => {
-    return Object.entries(forecast).filter(X => X).map(([day, forecastData]) =>
-        <div>
-            <div className={style.day} key={day}>{day}</div>
-            {iterateTroughDays(forecast)}
-        </div>
-    )
-}
 
 export const WeatherForecast = () => {
 
+    const HOURS_PER_DAY = 24;
+
+    const iterateTroughDays = () => {
+        saveCurrentTimeShown((currentTimeShown + 1) % HOURS_PER_DAY)
+    };
+
+    const displayEachDay = (forecast) => {
+        return Object.entries(forecast).filter(X => X).map(([day, forecastData]) =>
+            <div>
+                <div className={style.day}
+                     key={`${day}timestamp`}>{day} {forecastData[currentTimeShown].timestamp}</div>
+                <WeatherDataDisplay key={`${day}data`} weather={forecastData[currentTimeShown]}/>
+            </div>
+        )
+    }
+
     const [forecast, saveForecast] = useState(undefined);
+    const [currentTimeShown, saveCurrentTimeShown] = useState(0);
 
     const getForecast = async () => fetchWeatherForecast().then(data => parseForecast(data)).then(data => saveForecast(data));
 
     useEffect(() => {
         if (forecast === undefined) getForecast();
-        setInterval(() => {
+        const getForecastInterval = setInterval(() => {
             getForecast();
         }, 1000 * 60 * 60);
-        setInterval(() => {
+        const iterateTroughDaysInterval = setInterval(() => {
             iterateTroughDays();
-        }, 1000 * 1 / 2);
+        }, 1000);
+        return () => {
+            clearInterval(getForecastInterval);
+            clearInterval(iterateTroughDaysInterval);
+        }
     })
 
     return (
