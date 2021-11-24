8b05d86c2701edfe93c520b86593fb329680b849
Julius Figge <julius@figge.eu>
2021-11-24 15:51:51 +0100

    include date in clock

--- src/clock/Clock.jsx
+++ src/clock/Clock.jsx
@@ -5,19 +5,22 @@ import {TIME_LOCALE} from "../helper/config";
 export const Clock = () => {
 
     const [time, setTime] = useState();
+    const [date, setDate] = useState();
 
-    const buildDateString = date => date.toLocaleTimeString(TIME_LOCALE, {hour12: false});
+    const buildTimeString = dateTime => dateTime.toLocaleTimeString(TIME_LOCALE, {hour12: false});
+    const buildDateString = dateDate => dateDate.toLocaleDateString(TIME_LOCALE);
 
     useEffect(() => {
         setInterval(() => {
-            setTime(buildDateString(new Date()));
+            setTime(buildTimeString(new Date()));
+            setDate(buildDateString(new Date()));
         }, 1000);
     })
 
     return (
         <div className={style.clock}>
             <div className={style.time}>{time}</div>
-            {/*TODO add date*/}
+            <div className={style.date}>{date}</div>
         </div>
     );
 };
