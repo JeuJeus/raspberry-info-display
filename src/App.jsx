34d9a9fc2970c2603dedcf70181816c140249d76
Julius Figge <julius@figge.eu>
2021-11-24 07:49:50 +0100

    programatically switch between components react

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
 import style from './App.module.css';
 import {Clock} from "./clock/Clock";
-import {BrowserRouter, Outlet, Route, Routes, Switch, useLocation, useNavigate, useRoutes} from "react-router-dom";
+import {
+    BrowserRouter,
+    matchPath,
+    Outlet,
+    Route,
+    Routes,
+    Switch,
+    useLocation,
+    useNavigate,
+    useRoutes
+} from "react-router-dom";
 import {useEffect} from "react";
 import {WeatherCurrent} from "./weather/current/WeatherCurrent";
 import {WeatherForecast} from "./weather/forecast/WeatherForecast";
 
 export const App = () => {
 
-    const routes = useRoutes(
-        [
-            {
-                path: '/weather/current',
-                element: <WeatherCurrent/>
-            },
-            {
-                path: '/weather/forecast',
-                element: <WeatherForecast/>
-            }
-        ]
-    );
+    const pathComponentMapping = [
+        {
+            path: '/weather/current',
+            element: <WeatherCurrent/>
+        },
+        {
+            path: '/weather/forecast',
+            element: <WeatherForecast/>
+        }
+    ];
+    const routes = useRoutes(pathComponentMapping);
 
-    // const navigate = useNavigate();
-    // const pathname = navigate.pathname;
+    const navigate = useNavigate();
+    const {pathname} = useLocation();
 
-    // const switchComponent = () => {
-    //     const currentComponentIndex = paths.findIndex(pathname)
-    // const nextIndex = paths[(currentComponentIndex + 1) % paths.length];
-    // navigate.push(nextIndex)
-    // };
+    const switchComponent = () => {
+        const currentComponentIndex = pathComponentMapping.findIndex(r => r.path === pathname);
+        const nextIndex = pathComponentMapping[(currentComponentIndex + 1) % pathComponentMapping.length];
+        navigate(nextIndex.path);
+    };
 
-    // useEffect(() => {
-    //     let switchComponentInterval = setInterval(() => {
-    //         switchComponent();
-    //     }, 1000 * 30);
-    //     return () => clearInterval(switchComponentInterval);
-    // })
+    useEffect(() => {
+        let switchComponentInterval = setInterval(() => {
+            switchComponent();
+        }, 1000 * 30);
+        return () => clearInterval(switchComponentInterval);
+    })
 
     return (
         <div className={style.app}>
