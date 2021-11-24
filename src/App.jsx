ef0c04d05af9d6493a791ee88c9ef33cdfcba8e4
Julius Figge <julius@figge.eu>
2021-11-24 07:37:19 +0100

    WIP introduce routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import style from './App.module.css';
 import {Clock} from "./clock/Clock";
+import {BrowserRouter, Outlet, Route, Routes, Switch, useLocation, useNavigate, useRoutes} from "react-router-dom";
+import {useEffect} from "react";
 import {WeatherCurrent} from "./weather/current/WeatherCurrent";
 import {WeatherForecast} from "./weather/forecast/WeatherForecast";
 
-export const App = () => (
-    <div className={style.app}>
-        <Clock/>
-        {/*<WeatherCurrent/>*/}
-        <WeatherForecast/>
-    </div>
-);
+export const App = () => {
+
+    const routes = useRoutes(
+        [
+            {
+                path: '/weather/current',
+                element: <WeatherCurrent/>
+            },
+            {
+                path: '/weather/forecast',
+                element: <WeatherForecast/>
+            }
+        ]
+    );
+
+    // const navigate = useNavigate();
+    // const pathname = navigate.pathname;
+
+    // const switchComponent = () => {
+    //     const currentComponentIndex = paths.findIndex(pathname)
+    // const nextIndex = paths[(currentComponentIndex + 1) % paths.length];
+    // navigate.push(nextIndex)
+    // };
+
+    // useEffect(() => {
+    //     let switchComponentInterval = setInterval(() => {
+    //         switchComponent();
+    //     }, 1000 * 30);
+    //     return () => clearInterval(switchComponentInterval);
+    // })
+
+    return (
+        <div className={style.app}>
+            <Clock/>
+            {routes}
+        </div>
+    );
+}
+
+
+
